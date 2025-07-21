f17fdb8f2ee96ff7e13226fc531b7ea73d8593ce
Cesar Emmanuel <cesaremmmanul@gmail.com>
2025-07-21 18:48:33 -0300

    chore: update home page message

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home() {
-  return <h1>A vida é 10% o que acontece com você e 90% como você reage a isso.</h1>
+  return <h1>A vida é 10% o que acontece com você e 90% como você reage a isso. 😎</h1>
 }
 
 export default Home
