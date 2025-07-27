f0e77bcfc4055620b8d2603366486c8c982ce65c
Cesar Emmanuel <cesaremmmanul@gmail.com>
2025-07-27 18:57:49 -0300

    feat: add scripts `lint:check` & `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,9 @@
 function Home() {
-  return <h1>A vida é 10% o que acontece com você e 90% como você reage a isso. 😎</h1>
+  return (
+    <h1>
+      A vida é 10% o que acontece com você e 90% como você reage a isso. 😎
+    </h1>
+  );
 }
 
-export default Home
+export default Home;
