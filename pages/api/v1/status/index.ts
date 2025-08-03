7ff088a3d64b26411149b418da00490de4aa44bc
Cesar Emmanuel <cesaremmmanul@gmail.com>
2025-08-03 16:28:43 -0300

    feat: add database connection

--- pages/api/v1/status/index.ts
+++ pages/api/v1/status/index.ts
@@ -1,6 +1,10 @@
+import database from "../../../../infra/database";
+
 import type { NextApiRequest, NextApiResponse } from "next";
 
-function status(request: NextApiRequest, response: NextApiResponse) {
+async function status(request: NextApiRequest, response: NextApiResponse) {
+  const result = await database.query("SELECT 1 + 1;");
+  console.log(result);
   response.status(200).json({ chave: "valor" });
 }
 
