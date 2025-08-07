68667d8047d5f01e624f97176d501503dd2db6a0
Cesar Emmanuel <cesaremmmanul@gmail.com>
2025-08-07 20:02:01 -0300

    feat: finish `api/v1/status` endpoint

--- pages/api/v1/status/index.ts
+++ pages/api/v1/status/index.ts
@@ -3,9 +3,29 @@ import database from "infra/database";
 import type { NextApiRequest, NextApiResponse } from "next";
 
 async function status(request: NextApiRequest, response: NextApiResponse) {
-  const result = await database.query("SELECT 1 + 1;");
-  console.log(result);
-  response.status(200).json({ chave: "valor" });
+  const updatedAt = new Date().toISOString();
+  const databaseQueryVersion = await database.query("SHOW server_version;");
+  const databaseVersion = databaseQueryVersion.rows[0].server_version;
+
+  const maxConnectionsQuery = await database.query("SHOW max_connections;");
+  const maxConnections = maxConnectionsQuery.rows[0].max_connections;
+  const databaseName = process.env.POSTGRES_DB;
+  const maxConnectionsActiveQuery = await database.query({
+    text: "SELECT COUNT(*)::int FROM pg_stat_activity WHERE datname = $1;",
+    values: [databaseName],
+  });
+  const maxConnectionsActive = maxConnectionsActiveQuery.rows[0].count;
+
+  response.status(200).json({
+    updated_at: updatedAt,
+    dependencies: {
+      database: {
+        version: databaseVersion,
+        max_connections: parseInt(maxConnections),
+        opened_connections: maxConnectionsActive,
+      },
+    },
+  });
 }
 
 export default status;
