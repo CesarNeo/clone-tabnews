c8b36c27ed841bdce70dc2ead5bc3864b82acfca
Cesar Emmanuel <cesaremmmanul@gmail.com>
2025-08-08 17:23:26 -0300

    chore: add client.connect() on try catch block

--- infra/database.ts
+++ infra/database.ts
@@ -16,13 +16,14 @@ async function query(queryObject: TDatabaseQuery) {
     password: process.env.POSTGRES_PASSWORD,
   });
 
-  await client.connect();
-
   try {
+    await client.connect();
+
     const result = await client.query(queryObject);
     return result;
   } catch (error) {
     console.error("Database query error:", error);
+    throw error;
   } finally {
     await client.end();
   }
